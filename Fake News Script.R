c0b0ab39c3079f009a7cc143bd79de8533917cb4
Benjamin Anderson <andersonb97@gmail.com>
2020-09-28 16:44:51 -0600

    Initialized the data and read it in.

---
+++ Fake News Script.R
@@ -0,0 +1,11 @@
+################################
+# Fake News Kaggle Competition #
+################################
+
+## Load necessary packages
+library(dplyr)
+library(tidyverse)
+
+## Read in the data
+test <- read_csv('test.csv')
+train <- read_csv('train.csv')
